44ea5c37adaa400deed39a3652460c1cc6e5c327
Leslie Lopez <leslie2515lopez@gmail.com>
2022-07-15 21:22:13 -0500

    Added freeze function

--- app.js
+++ app.js
@@ -78,5 +78,24 @@ document.addEventListener("DOMContentLoaded", () => {
     undraw();
     currentPosition += width;
     draw();
+    freeze();
+  }
+
+  // FREEZE FUNCTION
+  function freeze() {
+    if (
+      current.some((index) =>
+        squares[currentPosition + index + width].classList.contains("taken")
+      )
+    ) {
+      current.forEach((index) =>
+        squares[currentPosition + index].classList.add("taken")
+      );
+      // START A NEW TETROMINO FALLING
+      random = Math.floor(Math.random() * theTetrominoes.length);
+      current = theTetrominoes[random][currentRotation];
+      currentPosition = 4;
+      draw();
+    }
   }
 });
