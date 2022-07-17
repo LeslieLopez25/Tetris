8f6d501f4c06aa5ac9046c25846ca0545d1c1e7f
Leslie Lopez <leslie2515lopez@gmail.com>
2022-07-17 17:57:23 -0500

    Added nextshape shown on mini-grid

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const ScoreDisplay = document.querySelector("#score");
   const StartBtn = document.querySelector("#start-button");
   const width = 10;
+  let nextRandom = 0;
 
   // THE TETROMINOES
   const lTetromino = [
@@ -106,10 +107,12 @@ document.addEventListener("DOMContentLoaded", () => {
         squares[currentPosition + index].classList.add("taken")
       );
       // START A NEW TETROMINO FALLING
-      random = Math.floor(Math.random() * theTetrominoes.length);
+      random = nextRandom;
+      nextRandom = Math.floor(Math.random() * theTetrominoes.length);
       current = theTetrominoes[random][currentRotation];
       currentPosition = 4;
       draw();
+      displayShape();
     }
   }
 
@@ -163,5 +166,29 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     current = theTetrominoes[random][currentRotation];
     draw();
+  }
+
+  // SHOW UP-NEXT TETROMINO IN MINI-GRID DISPLAY
+  const displaySquares = document.querySelectorAll(".mini-grid div");
+  const displayWidth = 4;
+  let displayIndex = 0;
+
+  const upNextTetrominoes = [
+    [1, displayWidth + 1, displayWidth * 2 + 1, 2], //lTetromino
+    [0, displayWidth, displayWidth + 1, displayWidth * 2 + 1], //zTetromino
+    [1, displayWidth, displayWidth + 1, displayWidth + 2], //tTetromino
+    [0, 1, displayWidth, displayWidth + 1], //oTetromino
+    [1, displayWidth + 1, displayWidth * 2 + 1, displayWidth * 3 + 1], //iTetromino
+  ];
+
+  // DISPLAY THE SHAPE IN THE MINI-GRID DISPLAY
+  function displayShape() {
+    // REMOVE ANY TRACE OF A TETROMINO FORM THE ENTIRE GRID
+    displaySquares.forEach((square) => {
+      square.classList.remove("tetromino");
+    });
+    upNextTetrominoes[nextRandom].forEach((index) => {
+      displaySquares[displayIndex + index].classList.add("tetromino");
+    });
   }
 });
